c0c0475c6c0bc20b77d6d512009d6c77ec37770e
“Jerry <jerryno153@gmail.com>
2024-11-11 12:23:21 +0800

    Initial Commit

---
+++ Bus_Schedule/Bus_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bus_ScheduleApp.swift
+//  Bus_Schedule
+//
+//  Created by 卢浩然 on 2024/11/11.
+//
+
+import SwiftUI
+
+@main
+struct Bus_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
